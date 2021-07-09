ce243e21fbdb814c8aa87573afa16812d4a929ec
Sanchit Bhatnagar <sanbht23@gmail.com>
2021-07-09 16:39:18 +0530

    Added Constants

---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+export {default as LANGUAGES} from "./languages";
+
+export const EXT_BASE_API = 'https://api.dictionaryapi.dev/api/v2/entries/'
---
+++ src/constants/languages.ts
@@ -0,0 +1,54 @@
+export default {
+    english_us: {
+        code: "en_US",
+        text: "English (US)"
+    },
+    hindi: {
+        code: "hi",
+        text: "Hindi"
+    },
+    spanish: {
+        code: "es",
+        text: "Spanish"
+    },
+    french: {
+        code: "fr",
+        text: "French"
+    },
+    japanese: {
+        code: "ja",
+        text: "Japanese"
+    },
+    russian: {
+        code: "ru",
+        text: "Russian"
+    },
+    english_uk: {
+        code: "en_GB",
+        text: "English (UK)"
+    },
+    german: {
+        code: "de",
+        text: "German"
+    },
+    italian: {
+        code: "it",
+        text: "Italian"
+    },
+    korean: {
+        code: "ko",
+        text: "Korean"
+    },
+    brazilian_portuguese: {
+        code: "pt-BR",
+        text: "Brazilian Portuguese"
+    },
+    arabic: {
+        code: "ar",
+        text: "Arabic"
+    },
+    turkish: {
+        code: "tr",
+        text: "Turkish"
+    },
+}
